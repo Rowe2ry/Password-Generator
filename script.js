116cb86755b2d539cd8f06b91a0ad253215b7c5c
Chris Rowe <rowe2ry@yahoo.com>
2022-03-23 16:43:13 -0400

    Error handler in script is passing all tests. Generator stills needs to be written.

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
+//simple function get a random character from any string passed into it
 function getRandom (charList) {
   charlist.charAt(Math.floor(Math.random() * charList.length));
 }
 
+//error handler if the user does not adhere to the prompt criteria
+function errorHandlerPassLength(userInputPassLength){
+// We check to see if the password is a number that walls between 8 and 128
+  if (userInputPassLength >= 8 && userInputPassLength <= 128) {
+  
+  console.log("Error handler check for password length - result:pass \n the users has chosen a " + userInputPassLength + " character long password.");
+  } else if (userInputPassLength < 8) {
+    window.alert("That number is too small. Passwords must be at least 8 characters long");
+    password = "This is NOT your password, you need to generate a new one while carefully reading the questions.";
+    console.log("Error!!! Password length input by user was too small.")
+    return password;
+  } else if (userInputPassLength > 128) {
+    window.alert("That number is too large. Passwords must be no more than 128 characters long");
+    password = "This is NOT your password, you need to generate a new one while carefully reading the questions.";
+    console.log("Error!!! Password length input by user was too big.")
+    return password;
+  } else {
+    window.alert("Password Generator has failed. \n Please respond to that question with a number from 8 to 128 next time.");
+    password = "This is NOT your password, you need to generate a new one while carefully reading the questions. ";
+    console.log("Error!!! Password length input by user was NaN.")
+    return password;
+  }
+}
+
 function generatePassword (){
-  var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   var numbercharacters = "0123456789";
   var capitalLetterCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercaseLetterCharacters = capitalLetterCharacters.toLocaleLowerCase();
   var validCharacterPool = "";
+  
   var passLengthDesired = window.prompt ("How many characters would you like in this password? (must be a number between 8 and 128)");
-  passLengthDesired = Number(passLengthDesired);
-  if (typeof passLengthDesired === typeof 3){
-    if (!(passLengthDesired >= 8 && passLengthDesired <= 128)) {
-      if (passLengthDesired < 8) {
-        window.alert("That number is too small. Passwords must be at least 8 characters long");
-      } else {
-        window.alert("That number is too large. Passwords must be no more than 128 characters long");
-      }
-      password = "This is NOT your password, you need to generate a new one while carefully reading the questions."
-      return password;
-    }
-    
-    password = "Just a placeholder test at the moment.";
-    return password;
-  } else {
-    console.log("Password Generator has failed. \n Please respond to that question with a number from 8 to 128 next time.");
-  } 
+  //converting the string input to a number
+
+  errorHandlerPassLength(Number(passLengthDesired))
+  password = "the error handler is working but the password generator is coming soon!"
+  return password;
   //window.confirm("Do you want special characters in this password")
 }
 
